06c223c7c34de99c8e4d2461e058bb867a27e532
bbb <bbb@local>
2026-01-21 18:15:29 +0100

    Test automatic version increment

--- public/version.js
+++ public/version.js
@@ -8,7 +8,7 @@
  */
 
 // Increment this version number when releasing updates
-const APP_VERSION = '1.7.7';
+const APP_VERSION = '1.7.8';
 
 // Make available globally for service worker
 self.APP_VERSION = APP_VERSION;
