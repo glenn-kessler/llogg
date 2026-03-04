63440d5c52197ade62092f09facdd598b8fa422f
bbb <bbb@local>
2026-03-04 13:41:08 +0100

    move ALL tests to folder tests/ rename test to match naming scheme remove directory tree from README.md

--- public/version.js
+++ public/version.js
@@ -8,7 +8,7 @@
  */
 
 // Increment this version number when releasing updates
-const APP_VERSION = '1.7.34';
+const APP_VERSION = '1.7.35';
 
 // Make available globally for service worker
 self.APP_VERSION = APP_VERSION;
